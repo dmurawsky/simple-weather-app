55048eaf488ca9d85e99ac66b9e07cd6f70a311d
Daniel Murawsky <d.r.murawsky@gmail.com>
2016-10-28 18:03:20 -0400

    final push for today

--- main.js
+++ main.js
@@ -2,18 +2,33 @@
 
     var fiveDay = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Atlanta,ga&units=imperial&cnt=5&appid=6b4fec3cfeb666ed226daff42cb09a95";
     var dayMap = ['today', 'tomorrow', 'thirdDay', 'fourthDay', 'fifthDay'];
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var imgs = {
+        "Clear":"img/art_clear.png",
+        "Clouds":"img/art_clouds.png",
+        "Fog":"img/art_fog.png",
+        "Light Clouds":"img/art_light_clouds.png",
+        "Light Rain":"img/art_light_rain.png",
+        "Rain":"img/art_rain.png",
+        "Snow":"img/art_snow.png",
+        "Storm":"img/art_storm.png"
+    };
     var json;
 
     // Call the weather API 
     ajax(fiveDay, function(resp){
         json = resp;
+        document.getElementById("loading").style.display = "none";
         for (var i=0; i<dayMap.length; i++){
             displayDetails(formatDetails(dayMap[i], json.list[i]));
         }
+        hashChange();
+        setFavicon(imgs[json.list[0].weather[0].main]);
     });
 
     // watches for the hash to change in the menu
-    window.addEventListener("hashchange", handleEvent);
+    window.addEventListener("hashchange", hashChange);
 
     // Populate the appropriate elements with the associated data
     function displayDetails(details){
@@ -28,12 +43,19 @@
     }
 
     // Format the data from the API in a an iterable format
+    function displayIcon(id, imgSrc){
+        var elem = document.getElementById(id);
+        if (elem) 
+            elem.src = imgSrc;
+    }
+
+    // Format the data from the API in a an iterable format
     function formatDetails(day, json){
         var obj = {};
         var d = new Date(0);
         d.setUTCSeconds(json.dt);
         if (day!=='today' && day!=='tomorrow') 
-            obj[day+"-weekday"] = weekday(d);
+            obj[day+"-weekday"] = weekdays[d.getDay()];
         obj[day+"-date"] = dateFormat(d);
         obj[day+"-description"] = json.weather[0].main;
         obj[day+"-high"] = Math.floor(json.temp.max)+'&deg;';
@@ -42,6 +64,7 @@
         obj[day+"-wind"] = Math.floor(json.speed)+" km/h";
         obj[day+"-direction"] = getWindDirection(json.deg);
         obj[day+"-pressure"] = Math.floor(json.pressure)+" hpa";
+        displayIcon(day+"-icon", imgs[json.weather[0].main]);
         return obj;
     }
 
@@ -54,41 +77,7 @@
 
     // Takes a date object and retruns a formated string
     function dateFormat(d){
-        return monthName(d.getMonth())+" "+d.getDate();
-    }
-
-    // Returns the month string associated with the getMonth() number
-    function monthName(num){
-        switch (num){
-            case 0: return "Jan";
-            case 1: return "Feb";
-            case 2: return "Mar";
-            case 3: return "Apr";
-            case 4: return "May";
-            case 5: return "Jun";
-            case 6: return "Jul";
-            case 7: return "Aug";
-            case 8: return "Sep";
-            case 9: return "Oct";
-            case 10: return "Nov";
-            case 11: return "Dec";
-            default: return "Invalid Month";
-        }
-    }
-
-    // Returns the weekday string associated with the getDay() number
-    function weekday(d){
-        var num = d.getDay();
-        switch (num){
-            case 0: return "Sunday";
-            case 1: return "Monday";
-            case 2: return "Tuesday";
-            case 3: return "Wednesday";
-            case 4: return "Thursday";
-            case 5: return "Friday";
-            case 6: return "Saturday";
-            default: return "Invalid Day";
-        }
+        return months[d.getMonth()]+" "+d.getDate();
     }
 
     // Creates and calls the XHR, then calls the callback
@@ -106,7 +95,7 @@
     }
 
     // Called on hashchange and changes views to and from home and detail pages based on location hash in the address bar
-    function handleEvent() {
+    function hashChange() {
         var hash = window.location.hash.slice(1);
         var ind = dayMap.indexOf(hash);
         console.log(hash, ind)
@@ -128,6 +117,15 @@
     function homePage(){
         document.getElementById("homepage").style.display = "block";
         document.getElementById("detailpage").style.display = "none";
+    }
+    
+    // Sets the favicon to todays forcast icon
+    function setFavicon(imgSrc) {
+        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = imgSrc;
+        document.getElementsByTagName('head')[0].appendChild(link);
     }
 })();
     
